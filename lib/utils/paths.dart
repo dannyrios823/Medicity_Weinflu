34d85f518426ea8bceb1f72736e091014c4e5010
dfrios <dfrios@bancolombia.com.co>
2023-12-29 17:07:58 -0500

    se modifica el nombre de las imagenes en ingles y se actualiza el splash

--- lib/utils/paths.dart
+++ lib/utils/paths.dart
@@ -1,7 +1,7 @@
 class RetoPaths {
   RetoPaths._();
-  static const String redImage = 'assets/images/rojo.png';
-  static const String blueImage = 'assets/images/azul.png';
-  static const String greenImage = 'assets/images/verde.png';
-  static const String purpleImage = 'assets/images/purpura.png';
+  static const String redImage = 'assets/images/red_pill.png';
+  static const String blueImage = 'assets/images/blue_pill.png';
+  static const String greenImage = 'assets/images/green_pill.png';
+  static const String purpleImage = 'assets/images/purple_pill.png';
 }
